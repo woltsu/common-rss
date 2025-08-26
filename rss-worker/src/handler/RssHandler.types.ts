fb90c400a6b8dc52bc5fac5f5d09c3b7e665d733
Olli Warro <olli.warro@gmail.com>
2025-08-26 20:18:17 +0300

    rm comment

--- rss-worker/src/handler/RssHandler.types.ts
+++ rss-worker/src/handler/RssHandler.types.ts
@@ -1,5 +1,3 @@
 import { RedisMessage } from '@common-rss/shared';
 
 export type RssHandler = (message: RedisMessage) => Promise<boolean>;
-
-/* export type RssHandlers = Record<RedisMessage['type'], RssHandler>; */
